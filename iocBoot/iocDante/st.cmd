48315bf5c48bf75d80c5c2cb5f3e764929d93c34
Mark Rivers <rivers@cars.uchicago.edu>
2020-02-22 08:22:50 -0600

    New IP address

--- iocBoot/iocDante/st.cmd
+++ iocBoot/iocDante/st.cmd
@@ -28,7 +28,7 @@ epicsEnvSet("MAX_THREADS", "8")
 epicsEnvSet("EPICS_DB_INCLUDE_PATH", "$(ADCORE)/db")
 
 # DanteConfig(portName, ipAddress, numDetectors, maxBuffers, maxMemory)
-DanteConfig("$(PORT)",  164.54.160.232, 1, 0, 0)
+DanteConfig("$(PORT)", 164.54.160.181, 1, 0, 0)
 
 dbLoadTemplate("dante.substitutions")
 
