72feec38629229c0d5156e29b525467d2e0d81bb
Mark Rivers <rivers@cars.uchicago.edu>
2021-04-01 12:30:20 -0500

    Move dante8.substitutions; pass P, NCHAN, and TRACE_LEN in dbLoadTemplate

--- iocBoot/iocDante8/st.cmd
+++ iocBoot/iocDante8/st.cmd
@@ -24,13 +24,18 @@ epicsEnvSet("NCHANS", "2048")
 epicsEnvSet("CBUFFS", "500")
 # The maximum number of threads for plugins which can run in multiple threads
 epicsEnvSet("MAX_THREADS", "8")
+# The maximum number of channels in the MCA records
+epicsEnvSet("MCA_CHANS", "2048")
+# The maximum number of points in the ADC trace waveform records
+epicsEnvSet("TRACE_LEN", "16384")
+# The search path for database files
 # The search path for database files
 epicsEnvSet("EPICS_DB_INCLUDE_PATH", "$(ADCORE)/db")
 
 # DanteConfig(portName, ipAddress, numDetectors, maxBuffers, maxMemory)
-DanteConfig("$(PORT)", 164.54.160.186, 1, 0, 0)
+DanteConfig("$(PORT)", 164.54.160.186, 8, 0, 0)
 
-dbLoadTemplate("dante.substitutions")
+dbLoadTemplate($(DANTE)/db/"dante8.substitutions", "P=$(PREFIX), NCHAN=$(MCA_CHANS), TRACE_LEN=$(TRACE_LEN)")
 
 # Load all other plugins using commonPlugins.cmd
 < $(ADCORE)/iocBoot/commonPlugins.cmd
